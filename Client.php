056d7c8cc5f216d527401e33db12688c52217fc4
mma <18710839146@163.com>
2018-03-19 00:27:00 +0800

    edit

--- Client.php
+++ Client.php
@@ -3,15 +3,28 @@
 class Client
 {
     private $client;
+    //swoole的版本号
+    public $swoole_version = SWOOLE_VERSION;
 
     public function __construct()
     {
-        $this->client = new swoole_client(SWOOLE_SOCK_TCP);
+        //SWOOLE_SOCK_TCP 创建tcp socket
+        //SWOOLE_SOCK_TCP6 创建tcp ipv6 socket
+        //SWOOLE_SOCK_UDP 创建udp socket
+        //SWOOLE_SOCK_UDP6 创建udp ipv6 socket
+        //SWOOLE_SOCK_SYNC 同步客户端
+        //SWOOLE_SOCK_ASYNC 异步客户端
+        $this->client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_SYNC);
+
+        //set 设置客户端参数
+        $this->client->set([]);
+        //on 注册异步时间回调函数
+        $eventArray = ['connect', 'error', 'receive', 'close'];
+        $this->client->on();
     }
 
     public function connect()
     {
-        file_put_contents('./connect.txt','connect');
         if (!$this->client->connect("127.0.0.1", 9501, 1)) {
             echo "Connect Error";
         }
--- Server.php
+++ Server.php
@@ -7,18 +7,25 @@ class Server
         $this->serv = new swoole_server("0.0.0.0", 9501);
         $this->serv->set(array(
             'worker_num' => 1,   //一般设置为服务器CPU数的1-4倍
-            'daemonize' => 1,  //以守护进程执行
+            'daemonize' => false,  //以守护进程执行
             'max_request' => 10000,
             'dispatch_mode' => 2,
             'task_worker_num' => 8,  //task进程的数量
             "task_ipc_mode " => 3 ,  //使用消息队列通信，并设置为争抢模式
             //"log_file" => "log/taskqueueu.log" ,//日志
         ));
+        $this->serv->on('Connect', array($this, 'onConnect'));
         $this->serv->on('Receive', array($this, 'onReceive'));
         // bind callback
         $this->serv->on('Task', array($this, 'onTask'));
         $this->serv->on('Finish', array($this, 'onFinish'));
+        $this->serv->on('Close', array($this, 'onClose'));
         $this->serv->start();
+    }
+
+    public function onConnect($serv , $fd)
+    {
+        echo $fd;
     }
     public function onReceive( swoole_server $serv, $fd, $from_id, $data ) {
         //echo "Get Message From Client {$fd}:{$data}n";
@@ -32,6 +39,10 @@ class Server
     }
     public function onFinish($serv,$task_id, $data) {
         //echo "Task {$task_id} finishn";
+        //echo "Result: {$data}n";
+    }
+    public function onClose($serv,$task_id) {
+        echo "Task {$task_id} close";
         //echo "Result: {$data}n";
     }
 }
