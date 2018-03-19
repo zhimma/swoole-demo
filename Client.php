5c2641d1c21df53d3fcdd6fb46d9054b3edf4ff8
maxiongfei <18710839146@163.com>
2018-03-19 19:26:35 +0800

    'test'

--- Client.php
+++ Client.php
@@ -2,7 +2,12 @@
 
 class Client
 {
-    private $client;
+    /**
+     * client handle
+     *
+     * @var swoole_client
+     */
+    protected $client;
     //swoole的版本号
     public $swoole_version = SWOOLE_VERSION;
 
@@ -14,23 +19,44 @@ class Client
         //SWOOLE_SOCK_UDP6 创建udp ipv6 socket
         //SWOOLE_SOCK_SYNC 同步客户端
         //SWOOLE_SOCK_ASYNC 异步客户端
-        $this->client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_SYNC);
+        $this->client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
 
         //set 设置客户端参数
-        $this->client->set([]);
-        //on 注册异步时间回调函数
+//        $this->client->set([]);
+        //on 注册异步事件回调函数
         $eventArray = ['connect', 'error', 'receive', 'close'];
-        $this->client->on();
+        foreach ($eventArray as $value) {
+            $this->client->on($value, array($this, $value));
+        }
+        $this->client->connect("127.0.0.1", 9501);
     }
 
     public function connect()
     {
-        if (!$this->client->connect("127.0.0.1", 9501, 1)) {
-            echo "Connect Error";
+        echo "connect \n";
+        $this->client->send('hello swool');
+    }
+    public function error($cli)
+    {
+        print_r($cli);
+    }
+
+    public function receive($cli , $data = '')
+    {
+        if(empty($data)){
+            $this->close();
+        }else {
+            echo "received : $data\n";
+            sleep(1);
+            $this->client->send("hello \n");
         }
-        $this->client->send('it\'s  from client');
+    }
+
+    public function close()
+    {
+        $this->client->close();
+        echo 'close' . "\n";
     }
 }
 
 $client = new Client();
-$client->connect();
--- Server.php
+++ Server.php
@@ -2,48 +2,72 @@
 
 class Server
 {
-    private $serv;
-    public function __construct() {
-        $this->serv = new swoole_server("0.0.0.0", 9501);
-        $this->serv->set(array(
-            'worker_num' => 1,   //一般设置为服务器CPU数的1-4倍
-            'daemonize' => false,  //以守护进程执行
-            'max_request' => 10000,
-            'dispatch_mode' => 2,
+    private $server;
+
+    public function __construct()
+    {
+        //new swoole_server
+        $this->server = new swoole_server("0.0.0.0", 9501);
+        //$server->on 设置事件回调
+        $event = ['Connect', 'Receive', 'Task', 'Finish', 'Close'];
+        foreach ($event as $value) {
+            $this->server->on($value, array($this, 'on' . $value));
+        }
+        //$server->set 设置运行参数
+        $this->server->set(array(
+            'worker_num'      => 1,   //一般设置为服务器CPU数的1-4倍
+            'daemonize'       => false,  //以守护进程执行
+            'max_request'     => 10000,
+            'dispatch_mode'   => 2,
             'task_worker_num' => 8,  //task进程的数量
-            "task_ipc_mode " => 3 ,  //使用消息队列通信，并设置为争抢模式
+            "task_ipc_mode "  => 3,  //使用消息队列通信，并设置为争抢模式
             //"log_file" => "log/taskqueueu.log" ,//日志
         ));
-        $this->serv->on('Connect', array($this, 'onConnect'));
-        $this->serv->on('Receive', array($this, 'onReceive'));
-        // bind callback
-        $this->serv->on('Task', array($this, 'onTask'));
-        $this->serv->on('Finish', array($this, 'onFinish'));
-        $this->serv->on('Close', array($this, 'onClose'));
-        $this->serv->start();
+        //$server->start启动服务器
+        $this->server->start();
     }
 
-    public function onConnect($serv , $fd)
+    public function onConnect($server, $fd)
     {
         echo $fd;
     }
-    public function onReceive( swoole_server $serv, $fd, $from_id, $data ) {
-        //echo "Get Message From Client {$fd}:{$data}n";
-        // send a task to task worker.
-        file_put_contents('./client.txt',$data);
-        $serv->task( $data );
-    }
-    public function onTask($serv,$task_id,$from_id, $data) {
-        file_put_contents('./client.txt',$data);
 
+    public function onReceive(swoole_server $server, $fd, $from_id, $data)
+    {
+        $server->task($data);
     }
-    public function onFinish($serv,$task_id, $data) {
+
+    public function onTask($server, $task_id, $from_id, $data)
+    {
+        echo $data;
+    }
+
+    public function onFinish($server, $task_id, $data)
+    {
         //echo "Task {$task_id} finishn";
         //echo "Result: {$data}n";
     }
-    public function onClose($serv,$task_id) {
+
+    public function onClose($server, $task_id)
+    {
         echo "Task {$task_id} close";
         //echo "Result: {$data}n";
     }
+
+    public function onStart()
+    {
+        echo 111;
+    }
+
+    public function onWorkerStart()
+    {
+        echo 222;
+    }
+
+    public function onTaskerStart()
+    {
+        echo 333;
+    }
 }
+
 $server = new Server();
