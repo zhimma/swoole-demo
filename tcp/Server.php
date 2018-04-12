8a2fe7355a1beb5e54f11079d588165771c391bb
maxiongfei <18710839146@163.com>
2018-04-12 12:11:51 +0800

    edit

--- tcp/Server.php
+++ tcp/Server.php
@@ -16,22 +16,24 @@ class Server
         // new swoole_server
         $this->server = new swoole_server("0.0.0.0", 9501);
         // $server->on 设置事件回调
-        $event = ['Start', 'Connect', 'Receive', 'Close' ,'Task' ,'Finish'];
+        $event = ['ManagerStart', 'WorkerStart', 'Start', 'Connect', 'Receive', 'Close', 'Task', 'Finish'];
         foreach ($event as $value) {
             $this->server->on($value, [$this, 'on' . $value]);
         }
         // $server->set 设置运行参数
-        $this->server->set([
-                               'worker_num'      => 4,   //一般设置为服务器CPU数的1-4倍
-                               'daemonize'       => false,  //以守护进程执行,
-                               'task_worker_num' => 4 //任务工作进程数量。注意：如果设置了此参数，则必须设置onTask回调函数和onFinish回调函数，否则程序无法运行
-                           ]);
+        $this->server->set(
+            [
+                'worker_num'      => 4,   //一般设置为服务器CPU数的1-4倍
+                'daemonize'       => false,  //以守护进程执行,
+                'task_worker_num' => 2 //任务工作进程数量。注意：如果设置了此参数，则必须设置onTask回调函数和onFinish回调函数，否则程序无法运行
+            ]
+        );
         // $server->start启动服务器
         $this->server->start();
     }
 
     // master 进程启动后，fork出Manager进程，触发ManageStart
-    public function onManagerStart()
+    public function onManagerStart($server)
     {
         echo "manage server start" . PHP_EOL;
     }
@@ -39,7 +41,11 @@ class Server
     // manager 进程启动后，fork出work进程
     public function onWorkerStart($server, $workerId)
     {
-        echo "worker {$workerId} start" . PHP_EOL;
+        if ($server->taskworker) {
+            echo "task {$workerId} worker start" . PHP_EOL;
+        } else {
+            echo "worker {$workerId} start" . PHP_EOL;
+        }
     }
 
     // worker 进程死掉后，触发WorkerStop
@@ -76,7 +82,7 @@ class Server
         echo "received msg from client $fd,msg is : {$data}" . PHP_EOL;
         fwrite(STDOUT, "请回复消息：");
         $msg = trim(fgets(STDIN));
-        $server->send($fd, $msg  . ' from worker');
+        $server->send($fd, $msg . ' from worker');
 
         // 较为耗时的任务 交给TaskWorker处理
         /*$data = json_encode(['fd' => $fd, 'data' => $data]);
@@ -88,16 +94,18 @@ class Server
     {
         fwrite(STDOUT, "请回复消息：");
         $msg = trim(fgets(STDIN));
-        $result = json_decode($data,true);
+        $result = json_decode($data, true);
+
         // onTask可以return，也可以echo，当return时会触发onFinish函数
         return json_encode(['fd' => $result['fd'], 'msg' => $msg . ' from task worker']);
 
 
     }
+
     // Task worker 完成后调用
     public function onFinish($server, $task_id, $data)
     {
-        $result = json_decode($data,true);
+        $result = json_decode($data, true);
         $server->send($result['fd'], $result['msg'], $task_id);
         //echo "Result: {$data}n";
     }
